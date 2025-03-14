9fa8f930eec89c4ec6ad3ea4db64306f89af732b
Rogith-Dev <tdrrohit@gmail.com>
2025-03-14 09:38:03 +0530

    sample commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testapp';
+
+  images = [
+    'http://adorama.com/alc/wp-content/uploads/2021/04/photography-camera-learning-feature.jpg',
+    'https://images.unsplash.com/photo-1554080353-a576cf803bda?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGhvdG9ncmFwaHl8ZW58MHx8MHx8fDA%3D',
+    'http://adorama.com/alc/wp-content/uploads/2021/04/photography-camera-learning-feature.jpg',
+    'http://adorama.com/alc/wp-content/uploads/2021/04/photography-camera-learning-feature.jpg',
+    'http://adorama.com/alc/wp-content/uploads/2021/04/photography-camera-learning-feature.jpg'
+  ];
+
+  currentIndex = 0;
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
